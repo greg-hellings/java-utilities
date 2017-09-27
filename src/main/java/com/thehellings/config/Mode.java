dc75e5aa3b52256848d496560974499a642d3fc5
Gregory Hellings <greg.hellings@gmail.com>
2017-09-27 10:58:35 -0500

    Making more docs for the Mode enum

--- src/main/java/com/thehellings/config/Mode.java
+++ src/main/java/com/thehellings/config/Mode.java
@@ -1,5 +1,13 @@
 package com.thehellings.config;
 
+/**
+ * Just a listing of modes for application operation.
+ *
+ * <p>
+ *     An enumeration of some of the modes that a product can be running in. There are more, and as I come to need them
+ *     I might support more of them here. Feel free to ask me to support others, or add them to a PR if you want.
+ * </p>
+ */
 public enum Mode {
     DEVELOPMENT,
     PRODUCTION
